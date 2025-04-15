1e8284580991f0f57a2748d486ea9a2cefee8607
Bin Laden <204919318+heckerguy@users.noreply.github.com>
2025-04-14 21:12:51 -0400

    asdf

--- netlify/functions/redisConnect.js
+++ netlify/functions/redisConnect.js
@@ -17,6 +17,7 @@ exports.handler = async (event, context) => {
 
     // Find the shortcut with the matching uuid
     const matchingShortcut = shortcuts.find((shortcut) => {
+      console.log('Checking shortcut:', shortcut);
       const parsedShortcut = JSON.parse(shortcut);
       return parsedShortcut.uuid === uuid;
     });
